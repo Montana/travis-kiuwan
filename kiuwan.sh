c65815dda8e689c3fcb0af3ebfd9f3d4441907a1
Montana Mendy <montana@linux.com>
2021-12-02 14:15:04 -0800

    shell script

--- kiuwan.sh
+++ kiuwan.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-  echo $TRAVIS_BUILD_DIR
-  export APPNAME=$(basename $TRAVIS_BUILD_DIR)
-  export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
-  wget -v https://www.kiuwan.com/pub/analyzer/KiuwanLocalAnalyzer.zip
-  unzip KiuwanLocalAnalyzer.zip -d $HOME/.
-  $HOME/KiuwanLocalAnalyzer/bin/agent.sh --user $kiuwan_user --pass $kiuwan_password -s $TRAVIS_BUILD_DIR -n $APPNAME -l $TRAVIS_BUILD_ID -c
+  echo "$TRAVIS_BUILD_DIR"
+  echo "export APPNAME=$(basename $TRAVIS_BUILD_DIR)"
+  echo "export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)"
+  echo "wget -v https://www.kiuwan.com/pub/analyzer/KiuwanLocalAnalyzer.zip"
+  echo "unzip KiuwanLocalAnalyzer.zip -d $HOME/."
+  echo "$HOME/KiuwanLocalAnalyzer/bin/agent.sh --user $kiuwan_user --pass $kiuwan_password -s $TRAVIS_BUILD_DIR -n $APPNAME -l $TRAVIS_BUILD_ID -c"
